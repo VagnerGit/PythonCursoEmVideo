cdec88da69f88022b588a24121416cb6bdefeeb1
VagnerGit <vagnergit@gmail.com>
2021-05-25 14:58:15 -0300

    aula 06 ex003 e ex004

---
+++ aula_06_tipos_primitivos_e_aída_de_dados/desafio_003_somando_dois_números.py
@@ -0,0 +1,7 @@
+'''Exercício Python 3:
+ Crie um programa que leia dois números e mostre a soma entre eles.'''
+
+print('Vamos Somar dois Nº inteiros')
+n1 = int(input('Digite 1º nº: '))
+n2 = int(input('Digite 2º nº: '))
+print(f'A de {n1}+{n2}={n1+n2}')
---
+++ aula_06_tipos_primitivos_e_aída_de_dados/desafio_004_dissecando_uma_variavel.py
@@ -0,0 +1,14 @@
+'''Exercício Python 4:
+ Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e
+todas as informações possíveis sobre ele.'''
+
+algo = input('Digite algo: ')
+print(type(algo))
+print('Só tem espaço ', algo.isspace())
+print(f'O {algo} é alfabetico? ', algo.isalpha())
+print(f'O {algo} é alfabetico e numerico? ', algo.isalnum())
+print(f'O {algo} é Nº? ', algo.isnumeric())
+print(f'O {algo} é decimal? ', algo.isdecimal())
+print(f'O {algo} esta tudo em letra maiuscula? ', algo.isupper())
+print(f'O {algo} esta tudo em letra minuscula? ', algo.islower())
+print(f'O {algo} a 1º letra é maiuscula? ', algo.istitle())
