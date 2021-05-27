a5d9aeda636c3cd9938da7392468a6791b3cc8ab
VagnerGit <vagnergit@gmail.com>
2021-05-27 14:52:36 -0300

    ex011 a ex012

---
+++ desafio_011_Pintando_parede.py
@@ -0,0 +1,11 @@
+'''Exercício Python 11: Faça um programa que leia a largura e
+a altura de uma parede em metros, calcule a sua área e a quantidade de
+tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma
+área de 2 metros quadrados.'''
+
+l = float(input('Qual a medida da lagura da parede '))
+a = float(input('Qual a medida da Altura da parede '))
+p = l*a
+# cada litro de tinta cobre 2m2
+t = p/2
+print('A quantidade de tinta nessesaria é', t, 'l')
---
+++ desafio_012_calculando_descontos.py
@@ -0,0 +1,7 @@
+'''Exercício Python 12: Faça um algoritmo que leia o preço de um produto e
+mostre seu novo preço, com 5% de desconto.'''
+
+p = float(input('Qual é preço sem desconto? R$'))
+d = p*0.95
+print('Preço sem desconto R${:.2f} com 5% de desconto R${:.2f}'.format(p, d))
+
