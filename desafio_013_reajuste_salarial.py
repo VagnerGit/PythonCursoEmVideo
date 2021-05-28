5688ddece34603a6a7eead483b82e4eabcef3b11
VagnerGit <vagnergit@gmail.com>
2021-05-28 08:12:41 -0300

    ex013 a ex015

---
+++ desafio_013_reajuste_salarial.py
@@ -0,0 +1,7 @@
+'''Exercício Python 13:
+ Faça um algoritmo que leia o salário de um
+funcionário e mostre seu novo salário, com 15% de aumento.'''
+
+s = float(input("Quanto é o salário que vai receber 15% de aumento R$"))
+a = round(s*1.15)#round arredonda
+print('O novo salário é R$', a)
---
+++ desafio_014_conversor_de_temperaturas.py
@@ -0,0 +1,10 @@
+'''Exercício Python 14:
+ Escreva um programa que converta uma temperatura digitando em graus Celsius e
+converta para graus Fahrenheit.'''
+
+print('\033[4:31mConversor Celsius e converta para graus Fahrenheit\033[m')
+print()
+t = float(input('Digite a temperatura em Cº: '))
+f = 9*t / 5 + 32 #p/converte Cº p/ Fº
+print('\033[33mCº=Fº\033[m  '*10)
+print('{}{}Cº{} é igual {}{}Fº{}'.format('\033[36m', t, '\033[m', '\033[31m', f, '\033[m'))
