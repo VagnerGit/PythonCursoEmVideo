49816efa35683d6a0ef1f609650d6b3b9fce82a6
VagnerGit <vagnergit@gmail.com>
2021-05-28 09:12:41 -0300

    ex016

---
+++ desafio_016_quebrando_um_número.py
@@ -0,0 +1,11 @@
+'''Exercício Python 16:
+Crie um programa que leia um
+número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.'''
+
+'''import math
+n = float(input('Digite Nº com casas decimais = '))
+print(math.floor(n), math.ceil(n), round(n)) # ceil arredonda p/ cima, floor para baixo e round arredonda'''
+
+from math import trunc
+num = float(input('Digite um Nº com virgula: '))
+print('O valor de {} e sua parte inteira é {}'.format(num, trunc(num)))# int(num) ou trunc
